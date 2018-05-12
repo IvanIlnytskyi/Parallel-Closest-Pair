98a9c24a80d89022b45eadf04ca85200a608111f
IvanIlnytskyi <i.ilnytskyi@gmail.com>
2018-05-12 18:05:17 +0300

    added pair result return

--- ClosestPairParallel.h
+++ ClosestPairParallel.h
@@ -6,6 +6,8 @@
 #include <thread>
 #include <iostream>
 #include <random>
+#include <string>
+#include <sstream>
 
 struct Point
 {
@@ -17,14 +19,39 @@ struct Point
 	}
 };
 
+struct PointPair
+{
+	Point a;
+	Point b;
+};
+
+std::string to_string(const PointPair & pp)
+{
+	std::stringstream ss;
+	ss<< "( "<<pp.a.x<<" , "<<pp.a.y<<" ),( "<<pp.b.x << " , " << pp.b.y << " )";
+	return ss.str();
+}
+
 float points_distance(const Point & a, const Point & b)
 {
 	return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
 }
 
-float boundary_merge(const std::vector<Point> & points, const float left_distance, const float right_distance, const float median)
+float boundary_merge(const std::vector<Point> & points, const float left_distance, const float right_distance, 
+					const PointPair left_pair,const PointPair right_pair,const float median, PointPair & min_dist_pair)
 {
-	float min_dist = fmin(left_distance, right_distance);
+	float min_dist;
+	if (left_distance < right_distance)
+	{
+		min_dist_pair = left_pair;
+		min_dist = left_distance;
+	}
+	else
+	{
+		min_dist_pair = right_pair;
+		min_dist = right_distance;
+	}
+
 	std::vector<Point> M(points.size());
 	std::vector<Point>::iterator iter = std::copy_if(points.begin(), points.end(), M.begin(), [&](Point p) {return p.x >= median - min_dist && p.x <= median + min_dist; });
 	M.resize(std::distance(M.begin(), iter));
@@ -33,21 +60,24 @@ float boundary_merge(const std::vector<Point> & points, const float left_distanc
 		for (size_t j = 1; j < 8; ++j)
 		{
 			if (min_dist > points_distance(M[i], M[i + j]))
+			{
 				min_dist = points_distance(M[i], M[i + j]);
+				min_dist_pair = { M[i], M[i + j] };
+			}
 		}
 	}
 	return min_dist;
 }
 	
-std::vector<Point> parallel_closest_pair(const std::vector<Point> &, float &);
+std::vector<Point> parallel_closest_pair(const std::vector<Point> &, float &,PointPair &);
 
-void closest_pair_invoker(const std::vector<Point> & P, float & dist, std::vector<Point> & P1)
+void closest_pair_invoker(const std::vector<Point> & P, float & dist, PointPair & pair, std::vector<Point> & P1)
 {
-	P1 = parallel_closest_pair(P, dist);
+	P1 = parallel_closest_pair(P, dist, pair);
 }
 
 
-std::vector<Point> parallel_closest_pair(const std::vector<Point> & P, float & dist)
+std::vector<Point> parallel_closest_pair(const std::vector<Point> & P, float & dist, PointPair & result_pair)
 {
 	if (P.size() < 2)
 	{
@@ -66,34 +96,39 @@ std::vector<Point> parallel_closest_pair(const std::vector<Point> & P, float & d
 		std::vector<Point> R1;
 
 		float L_dist, R_dist;
-		
+		PointPair L_pair, R_pair;
+
 		//closest_pair_invoker(std::ref(L), std::ref(L_dist), std::ref(L1));
 		//closest_pair_invoker(std::ref(R), std::ref(R_dist), std::ref(R1));
 
 		
-		std::thread t1(closest_pair_invoker, std::ref(L), std::ref(L_dist), std::ref(L1));
-		std::thread t2(closest_pair_invoker, std::ref(R), std::ref(R_dist), std::ref(R1));
+		std::thread t1(closest_pair_invoker, std::ref(L), std::ref(L_dist),std::ref(L_pair), std::ref(L1));
+		std::thread t2(closest_pair_invoker, std::ref(R), std::ref(R_dist),std::ref(R_pair), std::ref(R1));
 		t1.join();
 		t2.join();
 	
 		std::vector<Point> mergedPoints(L1.size() + R1.size());
 		std::merge(L1.begin(), L1.end(), R1.begin(), R1.end(), mergedPoints.begin(), [](Point left, Point right) {return left.y < right.y; });
-		dist = boundary_merge(mergedPoints, L_dist, R_dist, median);
+		dist = boundary_merge(mergedPoints, L_dist, R_dist,L_pair, R_pair,median, result_pair);
 		
 		return mergedPoints;
 	}
 }
 
 
-float sequential_closest_pair(const std::vector<Point> & points)
+float sequential_closest_pair(const std::vector<Point> & points,PointPair & pair_points)
 {
 	float min = FLT_MAX;
+	pair_points = { {FLT_MAX,FLT_MAX },{-FLT_MAX,-FLT_MAX } };
 	for (int i = 0; i < points.size(); ++i)
 		for (int j = i + 1; j < points.size(); ++j)
 		{
 			float temp_dist = points_distance(points[i], points[j]);
 			if (temp_dist < min)
+			{
 				min = temp_dist;
+				pair_points = { points[i],points[j] };
+			}
 		}
 	return min;
 }
@@ -104,7 +139,7 @@ std::vector<Point> create_test_vector(const size_t size)
 	srand(static_cast <unsigned> (time(0)));
 	for (int i = 0; i < size; ++i)
 	{
-		float x = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX) - 0.5f) *1000.0f;
+		float x = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX) - 0.5f) * 1000.0f;
 		float y = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX) - 0.5f) * 1000.0f;
 		result_vec[i] = { x,y };
 	}
--- Main.cpp
+++ Main.cpp
@@ -9,10 +9,12 @@ void main()
 {
 	vector<Point> points = create_test_vector(2048);
 	float par_distance;
-	auto vec = parallel_closest_pair(points, par_distance);
-	float seq_distance = sequential_closest_pair(points);
-	cout << "Results:\nSequential algorithm result: " << seq_distance << "." << endl;
-	cout << "Parallel algorithm result: " << par_distance << "." << endl;
+	PointPair par_pair;
+	PointPair seq_pair;
+	auto vec = parallel_closest_pair(points, par_distance,par_pair);
+	float seq_distance = sequential_closest_pair(points,seq_pair);
+	cout << "Results:\n1)Sequential algorithm result: \n\ta) min_distance" <<"\n\t\t" <<seq_distance << "; \n\tb) pair of points: "<<"\n\t\t" <<to_string(par_pair) << endl;
+	cout << "2)Parallel algorithm result: \n\ta) min_distance" << "\n\t\t"<< par_distance << "; \n\tb) pair of points: " << "\n\t\t"<< to_string(par_pair)<< endl;
 	cout << "Do algorithms return same results? " << (fabs(seq_distance - par_distance)<EPSILON) << endl;
 	system("Pause");
 }
